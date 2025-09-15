d34ca3bd095a890c4b8be826097828a58762df7b
Sophie <maxlicatenby@gmail.com>
2025-09-14 20:41:39 -0400

    Parallel download and caching

--- drawmap.py
+++ drawmap.py
@@ -1,6 +1,7 @@
 import sys
-
+import tiledownloader
 import pygame
+import functools
 
 pygame.init()
 pygame.font.init()
@@ -8,12 +9,92 @@ pygame.font.init()
 font = pygame.font.SysFont("Arial", 18)
 small = pygame.font.SysFont("Courier New", 15)
 FLAGS = pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.RESIZABLE
-SIZE = WIDTH, LENGTH = (900, 600)
+SIZE = WIDTH, HEIGHT = (900, 600)
 
-ORIGIN = (41.83, -71.41)
-ZOOM = 10
+ORIGIN = (22.334496945981964, 91.82751129180363)
+ZOOM = 0
+REDRAW = True
 
 screen = pygame.display.set_mode(SIZE, FLAGS)
+
+tilecache = {}
+lowrescache = {}
+downloaded = set()
+
+backup_image = pygame.image.load("tiles\\backup.png")
+
+#@functools.lru_cache(maxsize=None)
+def load_image(z, x, y, fail=False):
+
+    if y < 0 or y > (2 ** z) - 1:
+        image = backup_image
+        return image
+
+    cachekey = f"{z}_{x}_{y}"
+    if cachekey in tilecache:
+        return tilecache[cachekey]
+
+    while x > ((2**z)-1):
+        #print("Wraparound")
+        x -= (2**z)
+    while x < 0:
+        #print("-Wraparound")
+        x += (2**z)
+
+    if f"{z}_{x}_{y}" not in downloaded:
+        tiledownloader.q.put((z, x, y))
+        downloaded.add(f"{z}_{x}_{y}")
+
+    filename = f"tiles\\Mapnik\\{z}\\{x}_{y}.png"
+    cachefilename = f"lowres\\Mapnik\\{z}\\{x}_{y}.png"
+    #print(filename)
+    try:
+        image = pygame.image.load(filename)
+        tilecache[cachekey] = image
+    except FileNotFoundError as ex:
+        if fail: raise ex
+        if cachekey in lowrescache:
+            image = lowrescache[cachekey]
+        try:
+            image = pygame.image.load(cachefilename)
+            image = pygame.transform.scale2x(image)
+            lowrescache[cachekey] = image
+        except FileNotFoundError as ex:
+            image = backup_image
+    return image
+
+def draw_tiles(screen):
+    global REDRAW
+    screen.fill((255, 255, 255))
+    center = WIDTH // 2, HEIGHT // 2
+
+    tilecountx = WIDTH // 256 + 2
+    tilecounty = HEIGHT // 256 + 2
+
+    basepoint = tiledownloader.point_on_tile(*ORIGIN, ZOOM)
+    basepoint = tuple(int(i * 256) for i in basepoint)
+
+    basetile = tiledownloader.location_to_tile(*ORIGIN, ZOOM)
+    #print(basepoint)
+    offset = tuple(int(j - i) for i, j in zip(basepoint, center))
+    #print(offset)
+    while offset[0] > 0:
+        offset = (offset[0] - 256, offset[1])
+        basetile = (basetile[0] - 1, basetile[1])
+    while offset[1] > 0:
+        offset = (offset[0], offset[1] - 256)
+        basetile = (basetile[0], basetile[1] - 1)
+
+    for xtile in range(tilecountx):
+        for ytile in range(tilecounty):
+            image = load_image(ZOOM, basetile[0] + xtile, basetile[1] + ytile)
+            tile_offset = (offset[0] + (xtile * 256), offset[1] + (ytile * 256))
+            screen.blit(image, tile_offset)
+            pygame.draw.rect(screen, (255,0,0), (tile_offset, (256,256)), 1)
+
+
+
+
 
 if __name__ == "__main__":
     pygame.display.set_caption("Intercity Rail Game")
@@ -21,8 +102,32 @@ if __name__ == "__main__":
     pygame.display.flip()
 
     while True:
+        if REDRAW:
+            draw_tiles(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
+            if event.type == pygame.VIDEORESIZE:
+                SIZE = WIDTH, HEIGHT = event.size
+
+            if event.type == pygame.KEYDOWN:
+                REDRAW = True
+                keydelta = (1/(2**ZOOM)) * 33
+                if event.key == pygame.K_UP:
+                    ZOOM -= 1
+                    if ZOOM < 0: ZOOM = 0
+                elif event.key == pygame.K_DOWN:
+                    ZOOM += 1
+                    if ZOOM > 19: ZOOM = 19
+
+                elif event.key == pygame.K_w:
+                    ORIGIN = (ORIGIN[0] + keydelta, ORIGIN[1])
+                elif event.key == pygame.K_s:
+                    ORIGIN = (ORIGIN[0] - keydelta, ORIGIN[1])
+                elif event.key == pygame.K_a:
+                    ORIGIN = (ORIGIN[0], ORIGIN[1] - keydelta)
+                elif event.key == pygame.K_d:
+                    ORIGIN = (ORIGIN[0], ORIGIN[1] + keydelta)
--- tiledownloader.py
+++ tiledownloader.py
@@ -4,6 +4,8 @@ import urllib.request
 import os, sys
 import time
 from tqdm import tqdm, trange
+from PIL import Image
+
 
 from queue import Queue
 from threading import Thread
@@ -30,7 +32,7 @@ def get_tile_corner(zoom, x, y):
     lat_deg = math.degrees(lat_rad)
     return (lat_deg, lon_deg)
 
-def download_tile(z, x, y, basepath="tiles"):
+def download_tile(z, x, y, basepath="tiles", cachepath="lowres"):
     #url = f"https://tile.openstreetmap.org/{z}/{x}/{y}.png"
     #style = "Mapnik"
     #url = f"https://a.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
@@ -43,6 +45,7 @@ def download_tile(z, x, y, basepath="tiles"):
     opener.addheaders = [('User-agent', 'Sophie Johnson Intercity Rail Game 0.0.1')]
     urllib.request.install_opener(opener)
     print(url)
+
     if not os.path.exists(basepath):
         os.makedirs(basepath)
     if not os.path.exists(os.path.join(basepath, style)):
@@ -50,15 +53,46 @@ def download_tile(z, x, y, basepath="tiles"):
     if not os.path.exists(os.path.join(basepath, style, str(z))):
         os.makedirs(os.path.join(basepath, style, str(z)))
 
-    urllib.request.urlretrieve(url, f"{basepath}\\{style}\\{z}\\{x}_{y}.png")
+    if not os.path.exists(cachepath):
+        os.makedirs(cachepath)
+    if not os.path.exists(os.path.join(cachepath, style)):
+        os.makedirs(os.path.join(cachepath, style))
+    if not os.path.exists(os.path.join(cachepath, style, str(z+1))):
+        os.makedirs(os.path.join(cachepath, style, str(z+1)))
+
+    filename = f"{basepath}\\{style}\\{z}\\{x}_{y}.png"
+    urllib.request.urlretrieve(url, filename)
     time.sleep(0.1)
 
+    hires = Image.open(filename)
+    tl = hires.crop((0,0,127,127))
+    tr = hires.crop((128,0,255,127))
+    bl = hires.crop((0,128,127,255))
+    br = hires.crop((128,128,255,255))
+
+
+    try: tl.save(f"{cachepath}\\{style}\\{z+1}\\{x*2}_{y*2}.png")
+    except FileExistsError: pass
+    try: tr.save(f"{cachepath}\\{style}\\{z+1}\\{x*2+1}_{y*2}.png")
+    except FileExistsError: pass
+    try: bl.save(f"{cachepath}\\{style}\\{z+1}\\{x*2}_{y*2+1}.png")
+    except FileExistsError: pass
+    try: br.save(f"{cachepath}\\{style}\\{z+1}\\{x*2+1}_{y*2+1}.png")
+    except FileExistsError: pass
+
 def get_tile(z, x, y, basepath="tiles", age=(60*60*24*7)):
+    if x < 0 or y < 0: return
+    if x > (2**z)-1 or y > (2**z)-1: return
+
     filename = f"{basepath}\\{style}\\{z}\\{x}_{y}.png"
     if not os.path.exists(filename):
         download_tile(z, x, y, basepath)
+        print(f"Downloaded {filename}, did not exist")
     if (time.time() - os.path.getmtime(filename)) > age:
         download_tile(z, x, y, basepath)
+        print(f"Downloaded {filename}, too old")
+    else:
+        print(f"{filename} exists within cache window")
 
 def location_to_tile(lat, lon, zoom):
     n = 2 ** zoom
@@ -66,6 +100,13 @@ def location_to_tile(lat, lon, zoom):
     lat = math.radians(lat)
     ytile = n * (1 - (math.log(math.tan(lat) + sec(lat)) / math.pi)) / 2
     return (int(xtile), int(ytile))
+
+def point_on_tile(lat, lon, zoom):
+    n = 2 ** zoom
+    xtile = n * ((lon + 180) / 360)
+    lat = math.radians(lat)
+    ytile = n * (1 - (math.log(math.tan(lat) + sec(lat)) / math.pi)) / 2
+    return xtile % 1, ytile % 1
 
 
 def generate_parent_tiles(z, x, y):
@@ -82,19 +123,18 @@ def worker(q):
         get_tile(z, x, y)
         q.task_done()
 
+q = Queue(maxsize=0)
+for i in range(16):
+    t = Thread(target=worker, args=(q,), daemon=True)
+    t.start()
 
 if __name__ == '__main__':
-    ORIGIN = (50.0327971, 14.3797134)
+    ORIGIN = (43.04256466450335, -70.93208046028336)
     ZOOM = 20
     firsttile = location_to_tile(ORIGIN[0], ORIGIN[1], ZOOM)
     all = list(generate_parent_tiles(ZOOM, firsttile[0], firsttile[1]))
     all.reverse()
     print(all)
-
-    q = Queue(maxsize=0)
-    for i in range(1):
-        t = Thread(target=worker, args=(q,), daemon=True)
-        t.start()
 
     for tile in all:
         q.put(tile)
